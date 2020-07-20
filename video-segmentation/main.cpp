f5a1352bfcca8457eefb4bd7c468a1a21ee85657
caesiumCode <caesiumcode@aol.com>
2020-07-20 16:37:01 +0200

    Initial Commit

---
+++ video-segmentation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  video-segmentation
+//
+//  Created by Stephen Jaud on 20/07/2020.
+//  Copyright © 2020 Stephen Jaud. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
