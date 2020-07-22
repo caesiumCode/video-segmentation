1646e021403e938cffc50ace8c045556b5acbe28
caesiumCode <caesiumcode@aol.com>
2020-07-22 19:40:01 +0200

    test with argc and argv

--- video-segmentation/main.cpp
+++ video-segmentation/main.cpp
@@ -6,9 +6,16 @@
 //  Copyright © 2020 Stephen Jaud. All rights reserved.
 //
 
+#include <string>
+#include <iostream>
+
 #include "Program.hpp"
 
 int main(int argc, const char * argv[]) {
+    std::string path;
+    for (int i = 0; i < argc; i++)
+        std::cout << argv[i] << std::endl;
+        
     Program program;
     program.run();
     
