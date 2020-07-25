9387eac2e230b04da26f1d4525048f65ee92104e
caesiumCode <caesiumcode@aol.com>
2020-07-25 18:04:32 +0200

    Setup window

--- video-segmentation/Program.cpp
+++ video-segmentation/Program.cpp
@@ -9,9 +9,61 @@
 #include "Program.hpp"
 
 Program::Program(std::string inputPath) {
+    loadImageset(inputPath);
     
+    window.create(sf::VideoMode(imageset_dim.x, imageset_dim.y), TITLE);
+    window.setFramerateLimit(30);
 }
 
 void Program::run() {
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        
+        window.clear();
+        window.display();
+    }
+}
+
+bool Program::isSupported(std::string ext) {
+    for (const auto& exti : SUPPORTED_IMAGE_FORMATS)
+        if (ext.compare(exti) == 0)
+            return true;
     
+    return false;
+}
+
+void Program::loadImageset(std::string inputPath) {
+    // - - - Initialise filenames - - -
+    std::vector<std::string> filenames = {};
+    
+    for (const auto& entry : fs::directory_iterator(inputPath)) {
+        std::string file_extension = entry.path().extension();
+        std::string file_name = entry.path().string();
+        if (isSupported(file_extension))
+            filenames.push_back(file_name);
+    }
+    
+    std::sort(filenames.begin(), filenames.end());
+    
+    // - - - Set imageset_size variable - - -
+    imageset_size = (int) filenames.size();
+    
+    // - - - Load imageset - - -
+    std::cout << "INFO: Loading imageset\n";
+    for (int i = 0; i < imageset_size; i++) {
+        sf::Image image;
+        if (!image.loadFromFile(filenames[i])) {
+            std::cout << "\nLOADING ERROR : " << filenames[i] << "\n";
+        } else {
+            imageset.push_back(image);
+        }
+    }
+    std::cout << "\n";
+    
+    // - - - Set imageset_dim variable - - -
+    imageset_dim = imageset[0].getSize();
 }
--- video-segmentation/Program.hpp
+++ video-segmentation/Program.hpp
@@ -9,7 +9,15 @@
 #ifndef Program_hpp
 #define Program_hpp
 
+#include <filesystem>
+#include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <SFML/Graphics.hpp>
+
+namespace fs = std::filesystem;
+
 
 class Program {
 public:
@@ -17,6 +25,17 @@ public:
     void run();
     
 private:
+    const std::vector<std::string> SUPPORTED_IMAGE_FORMATS = {".png", ".jpg", ".jpeg"};
+    const std::string TITLE = "Video Segmentation";
+    
+    bool isSupported(std::string);
+    void loadImageset(std::string);
+    
+    sf::RenderWindow window;
+    
+    std::vector<sf::Image> imageset;
+    int imageset_size;
+    sf::Vector2u imageset_dim;
 };
 
 #endif /* Program_hpp */
